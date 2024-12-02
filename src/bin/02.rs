1df4c58c2016fb135e0c7bbeb6d79981faf3bc29
yassine safraoui <yassine.safraoui@grenoble-inp.org>
2024-12-02 21:36:55 +0100

    day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -5,7 +5,7 @@ use const_format::concatcp;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const DAY: &str = "02"; // TODO: Fill the day
+const DAY: &str = "02";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
@@ -15,7 +15,7 @@ const TEST: &str = "\
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9
-"; // TODO: Add the test input
+";
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -74,7 +74,6 @@ fn main() -> Result<()> {
         Ok(safe_reports)
     }
 
-    // TODO: Set the expected answer for the test input
     assert_eq!(2, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
