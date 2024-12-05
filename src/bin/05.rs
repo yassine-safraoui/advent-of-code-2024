78ba91a7e0835d9243c7ccdb4f6136feaa8387cd
yassine safraoui <yassine.safraoui@grenoble-inp.org>
2024-12-05 16:47:56 +0100

    todos

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -6,7 +6,7 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const DAY: &str = "05"; // TODO: Fill the day
+const DAY: &str = "05";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
@@ -38,7 +38,7 @@ const TEST: &str = "\
 75,97,47,61,53
 61,13,29
 97,13,75,29,47
-"; // TODO: Add the test input
+";
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -108,7 +108,6 @@ fn main() -> Result<()> {
     println!("\n=== Part 2 ===");
 
     fn part2<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
         let mut result: usize = 0;
         let mut parsing_rules = true;
         let mut rules: HashSet<(usize, usize)> = HashSet::new();
